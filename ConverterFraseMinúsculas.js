b5ba0309315ee0df5c1f49c43949344def2c1f47
Thais Ribs <thais.ribs@live.com>
2025-10-31 01:27:51 -0300

    Add files via upload

---
+++ ConverterFraseMinúsculas.js
@@ -0,0 +1,4 @@
+let frase = prompt("Digite uma frase:");
+let fraseMaiusculo = frase.toUpperCase();
+
+alert("frase em maiúsculas: " + fraseMaiusculo);
---
+++ converterLetrasMaiúsculas.js
@@ -0,0 +1,4 @@
+let nome = prompt("Digite seu nome:");
+let nomeMaiusculo = nome.toUpperCase();
+
+alert("Nome em maiúsculas: " + nomeMaiusculo);
